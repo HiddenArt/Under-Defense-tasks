fb8119a68fa3602e0c419fdfae006b26cbc47724
Hidden Art <78607876+HiddenArt@users.noreply.github.com>
2022-06-04 21:24:53 +0300

    Add files via upload

---
+++ Windows course/script2/download_install_file.ps1
@@ -0,0 +1,12 @@
+﻿$url = "https://dl3.topfiles.net/files/2/1370/30671/ZHRBQXVqQBFOcHhvUGxoL1R0YlBtSGY3VHc0d211VmRLc0lOYi9pZVVoMzRZQT06OhG8UjjKQtQGt-SDI8KaTnY/arduino-ide_1.8.19.exe"
+$destination = "$PSScriptRoot/arduino-ide_1.8.19.exe"
+
+#.Net file Downloadload
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $destination)
+
+#BitsTransfer file Downloadload
+#Start-BitsTransfer -Source $url -Destination $destination
+
+#Install file
+Start-Process -Wait -FilePath "$PSScriptRoot/arduino-ide_1.8.19.exe" -ArgumentList "/S" -PassThru
