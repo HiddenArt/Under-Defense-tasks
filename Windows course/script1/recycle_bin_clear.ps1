49cfb68fa3b6a10439c14234ec8644a664274f3e
Hidden Art <78607876+HiddenArt@users.noreply.github.com>
2022-06-04 19:44:36 +0300

    Add files via upload

---
+++ Windows course/script1/recycle_bin_clear.ps1
@@ -0,0 +1,20 @@
+﻿
+$msgBody = "Recycle bin has been cleaned"
+$msgTitle = "Inform about cleaning"
+$msgButton = 'Ok'
+$msgImage = 'Information'
+
+#pop-up message
+#[System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+
+
+#notification balloon
+[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+[reflection.assembly]::loadwithpartialname('System.Drawing')
+$notify = new-object system.windows.forms.notifyicon
+$notify.icon = [System.Drawing.SystemIcons]::Information
+$notify.visible = $true
+$notify.showballoontip(10,$msgTitle,$msgBody,[system.windows.forms.tooltipicon]::Info)
+
+#Clear all recycle bins without confirmation
+Clear-RecycleBin -Force
---
+++ Windows course/script1/recycle_bin_scheldule.ps1
@@ -0,0 +1,23 @@
+﻿# create a new task action
+$taskAction = New-ScheduledTaskAction `
+    -Execute 'powershell.exe' `
+    -Argument '-File C:\Powershell_scripts\recycle_bin_clear.ps1'
+
+# create trigger (Every Monday at 15:00)
+$taskTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday -At 3PM
+$tasktrigger
+
+# Register the new PowerShell scheduled task
+
+# The name of scheduled task.
+$taskName = "RecycleBinClear"
+
+# Description the scheduled task.
+$description = "Clean recycle bin every Monday at 15:00"
+
+# Register the scheduled task
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $taskAction `
+    -Trigger $taskTrigger `
+    -Description $description
